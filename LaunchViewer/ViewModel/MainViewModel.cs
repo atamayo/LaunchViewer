174e182899562c1123ddf89e8db167e37429a798
11atamayo <alex.tamayo@sonova.com>
2020-07-14 23:56:44 +0200

    Skeleton

---
+++ LaunchViewer/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchViewer.ViewModel
+{
+    public class MainViewModel
+    {
+        public string MSG { get; set; }
+        public MainViewModel()
+        {
+            MSG = DateTime.Now.ToLongTimeString();
+
+
+        }
+    }
+}
